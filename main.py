9b1a5c46d75b572ecc8edfd3675d6ea659b2ad00
jualga02 <juan.ramon.alfa@gmail.com>
2026-04-06 08:33:59 +0200

    Seguridad terminada con base de datos 'fake' La inclusión de la base de datos será la siguiente tarea

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import shutil
-
-from fastapi import Depends, FastAPI, File, HTTPException, Path, Query, UploadFile
+import jwt
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends, FastAPI, File, HTTPException, Path, Query, UploadFile, status
 from typing import Annotated
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from pathlib import Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 UPLOAD_DIR = Path("./fotos")
 # UPLOAD_DIR.mkdir(exist_ok=True) crea la carpeta si no existe
+
+
+
 
 # Clase de la tabla Foto
 class Foto(SQLModel, table=True):
@@ -17,6 +27,8 @@ class Foto(SQLModel, table=True):
     comment: str | None = Field(index=True)
     # route: str | None = Field(index=True)
     user: str | None = Field(index=True)
+
+    
 
 # Motor del modelo
 sqlite_file_name = "album.db"
@@ -37,6 +49,142 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
+# ====================================================================
+# Security
+# ====================================================================
+SECRET_KEY = "a2c315dfbbc06c5e8571a69e8ef67492860313627195b2e23c9eb0fd2f75d42b"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$wagCPXjifgvUFBzq4hqe3w$CYaIb8sB+wtD+Vu/P4uod1+Qof8h+1g7bbDlBID48Rc",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$wagCPXjifgvUFBzq4hqe3w$CYaIb8sB+wtD+Vu/P4uod1+Qof8h+1g7bbDlBID48Rc",
+        "disabled": False,
+    },
+}
+
+# Clase Token
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None    
+
+
+
+# Clase User 
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disable: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str    
+
+password_hash = PasswordHash.recommended()
+DUMMY_HASH = password_hash.hash("dummypassword")
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+def get_password_hash(password):    
+    return password_hash.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        verify_password(password, DUMMY_HASH)
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})        
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate":"Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+
+
+async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)],):
+    if current_user.disable:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/users/me/")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)],) -> User:
+    return current_user
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+# END SECURITY CODE ====================================================================
 @app.get("/")
 def read_root():
     return {"Hola":"desde nuestro Album"}
